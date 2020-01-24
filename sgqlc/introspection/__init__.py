profusion/sgqlc

commit 146908e679d05b21bdc92955da3da61b6ae5ee6e

introspection: use stricter variable types
Seems hasura enforces the `Boolean!` variable type even if a non-null
variable is given.

Fixes #80

sgqlc/introspection/__init__.py
@@ -21,8 +21,8 @@ __docformat__ = 'reStructuredText en'
 #  - includeDeprecated: if deprecated fields should be included
 query = '''
 query IntrospectionQuery(
-  $includeDescription: Boolean = true,
-  $includeDeprecated: Boolean = true,
+  $includeDescription: Boolean! = true,
+  $includeDeprecated: Boolean! = true,
 ) {
   __schema {
     queryType { name }

commit fb13c5fed35f2f291df10c56e50b4a4028dc107e

introspection: do not define default value in the query itself
GitHub server doesn't accept a default value if the argument is
non-nullable.

This was a regression introduced by 146908e679d05b21bdc92955da3da61b6ae5ee6e

Fixes: #87

sgqlc/introspection/__init__.py
@@ -21,8 +21,8 @@ __docformat__ = 'reStructuredText en'
 #  - includeDeprecated: if deprecated fields should be included
 query = '''
 query IntrospectionQuery(
-  $includeDescription: Boolean! = true,
-  $includeDeprecated: Boolean! = true,
+  $includeDescription: Boolean!,
+  $includeDeprecated: Boolean!,
 ) {
   __schema {
     queryType { name }