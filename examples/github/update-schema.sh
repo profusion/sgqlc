96859964e1e04f5aad05649954387e6d05d0e25a
Gustavo Sverzut Barbieri <barbieri@profusion.mobi>
2021-01-30 01:43:32 -0300

    git hooks now test update-schema and update-operations

--- examples/github/update-schema.sh
+++ examples/github/update-schema.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-if [ -z "$GH_TOKEN" ]; then
-    echo "please export GH_TOKEN with your GitHub API token" >&2
-    echo "see: https://github.com/settings/tokens" >&2
-    exit 1
-fi
+if [ "$NO_DOWNLOAD" != 1 ]; then
+    if [ -z "$GH_TOKEN" ]; then
+        echo "please export GH_TOKEN with your GitHub API token" >&2
+        echo "see: https://github.com/settings/tokens" >&2
+        exit 1
+    fi
 
-python3 \
-    -m sgqlc.introspection \
-    --exclude-deprecated \
-    --exclude-description \
-    -H "Authorization: bearer ${GH_TOKEN}" \
-    https://api.github.com/graphql \
-    github_schema.json || exit 1
+    python3 \
+        -m sgqlc.introspection \
+        --exclude-deprecated \
+        --exclude-description \
+        -H "Authorization: bearer ${GH_TOKEN}" \
+        https://api.github.com/graphql \
+        github_schema.json || exit 1
+fi
 
 sgqlc-codegen schema github_schema.json github_schema.py
