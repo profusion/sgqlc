c27cd8bbc1ddb6a7bbcc91dad0d09d54271d867c
maaft <asdfg>
2021-03-19 10:22:27 +0100

    now really fix formatting

--- sgqlc/codegen/operation.py
+++ sgqlc/codegen/operation.py
@@ -262,8 +262,9 @@ class GraphQLToPython(Visitor):
                                              children, lines, idx):
         sel = self.selection_name(parent, '_as__%s' % type_condition, idx)
         idx += 1
-        lines.append('%s = %s.__as__(%s)' %
-                     (sel, parent, str(self.schema_name) + "." + type_condition))
+        full_type_condition = str(self.schema_name) + "." + type_condition
+        lines.append('%s = %s.__as__(%s)' % full_type_condition
+                     (sel, parent, ))
         return self.format_selection_set(sel, children, lines, idx)
 
     @staticmethod
