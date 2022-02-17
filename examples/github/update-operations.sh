8b2e514f3f5ed3b239078595c29a8d7bd28bc7bf
Gustavo Sverzut Barbieri <barbieri@profusion.mobi>
2022-02-17 10:43:06 -0300

    fix update-operations.sh to catch codegen errors

--- examples/github/update-operations.sh
+++ examples/github/update-operations.sh
@@ -4,6 +4,6 @@ sgqlc-codegen operation \
     --schema github_schema.json \
     github_schema \
     sample_operations.py \
-    sample_operations.gql
+    sample_operations.gql || exit 1
 
 python3 -c 'import sample_operations' || exit 1
--- examples/shopify/update-operations.sh
+++ examples/shopify/update-operations.sh
@@ -4,6 +4,6 @@ sgqlc-codegen operation \
     --schema shopify_schema.json \
     shopify_schema \
     shopify_operations.py \
-    shopify_operations.gql
+    shopify_operations.gql || exit 1
 
 python3 -c 'import shopify_operations' || exit 1
