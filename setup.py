467d724f460c5245fd0c5e668d7cd5a2707a42c5
Gustavo Sverzut Barbieri <barbieri@profusion.mobi>
2022-12-05 15:35:00 -0300

    bump version to v16.1
    fixes:
     - enum now handles 'null' (thanks to @kgadek)
     - codegen: sort by kind, fixes #189

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 name = 'sgqlc'
 version = 16
-release = 0
+release = 1
 
 
 def cleanup_rst(doc):
--- sgqlc/__init__.py
+++ sgqlc/__init__.py
@@ -25,4 +25,4 @@ modules:
 '''
 
 __docformat__ = 'reStructuredText en'
-__version__ = '16.0'
+__version__ = '16.1'
