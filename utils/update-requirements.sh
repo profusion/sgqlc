20c9d109607e568e27de3898d00551f0d036739a
Gustavo Sverzut Barbieri <barbieri@profusion.mobi>
2023-06-19 16:26:07 -0300

    pipenv update

--- utils/update-requirements.sh
+++ utils/update-requirements.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # we must keep dev-requirements.txt so we can test on other Python versions
-pipenv lock -r --dev > dev-requirements.txt
+pipenv requirements --dev > dev-requirements.txt
